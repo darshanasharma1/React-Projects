a67642d1966e75e9334a1ea231f90f8c1516a36a
darshanasharma1 <darshana.sharma@researchwire.in>
2025-09-02 21:10:46 +0530

    Add task-manager project

---
+++ task-manager/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import react from "react";
+
+import TaskManager from "./TaskManager";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <TaskManager />
+      </div>
+    </div>
+  );
+}
+
+export default App;
