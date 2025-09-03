fa505d7ec0b53e14f11153e055ea37c9ebb893da
darshanasharma1 <darshana.sharma@researchwire.in>
2025-09-03 14:20:28 +0530

    Fix merge conflict in ToggleTheme App.js

--- ToggleTheme/src/App.js
+++ ToggleTheme/src/App.js
@@ -1,35 +1,14 @@
 import logo from './logo.svg';
-<<<<<<< HEAD
 import react from "react";
 
 import TaskManager from "./TaskManager";
-=======
-import './App.css';
->>>>>>> Initialize project using Create React App
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <div>
         <TaskManager />
       </div>
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> Initialize project using Create React App
     </div>
   );
 }
