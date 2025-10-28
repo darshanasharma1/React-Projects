6fe6ee77feea8d9e362e548b5af2f2f91e17ca68
darshanasharma1 <darshana.sharma@researchwire.in>
2025-10-28 12:22:12 +0530

    Cleaned project structure — separated portfolio, ToggleTheme, and task-manager

--- task-manager/task-manager/ToggleTheme/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders theme toggle app', () => {
-  render(<App />);
-  // Change to match your actual content
-  const headingElement = screen.getByText(/React Project-6/i);
-  expect(headingElement).toBeInTheDocument();
-});
